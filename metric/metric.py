fc9659bc97b25c712888450fa0055f93f5d47323
Michał Lorens <Michal.Lorens@pega.com>
2018-05-27 11:46:16 +0200

    parts

--- metric/metric.py
+++
@@ -1,9 +0,0 @@
-def metric_learn(metric, model, data_set):
-    model.eval()
-    for batch, inputs in enumerate(data_set):
-        labels = inputs[0][0]
-        inputs = [i[1].cuda() for i in inputs]
-        inputs, _, _ = model(*inputs)
-        features = inputs.numpy()
-        labels = labels.numpy()
-        metric.fit(features, labels)
--- trainer.py
+++ trainer.py
@@ -1,3 +1,5 @@
+import copy
+
 import torch
 from torch.nn import PairwiseDistance
 from torch.utils.data import DataLoader
@@ -10,18 +12,24 @@ class Trainer:
     def __init__(self, name, model, data_set, optimizer, scheduler, criterion, plot, batch_size=64, max_epoch=50, log_interval=15):
         train, val, test = data_set
         self.batch_size = batch_size
-        self.train_set = DataLoader(train, batch_size=batch_size, shuffle=True, pin_memory=True, drop_last=True)
-        self.test_set = DataLoader(test, batch_size=batch_size, shuffle=False, pin_memory=True, drop_last=True)
-        self.validate_set = DataLoader(val, batch_size=batch_size, shuffle=False, pin_memory=False, drop_last=True)
-        self.distance = PairwiseDistance()
+
+        self.train_set = tuple([DataLoader(t, batch_size=batch_size, drop_last=True) for t in train])
+        self.train_batches = len(self.train_set[0])
+        self.train_len = len(self.train_set[0].dataset)
+        self.test_set = tuple([DataLoader(t, batch_size=batch_size, drop_last=True) for t in test])
+        self.test_batches = len(self.test_set[0])
+        self.validate_set = tuple([DataLoader(v, batch_size=batch_size, drop_last=True) for v in val])
+        self.validate_len = len(self.validate_set[0].dataset)
+
+        self.dist = PairwiseDistance()
         self.optimizer = optimizer
         self.scheduler = scheduler
         self.criterion = criterion
-        self.name = name
         self.model = model
         self.plot = plot
         self.max_epoch = max_epoch
         self.log_interval = log_interval
+        self.name = name
         self.best_accuracy = 0
 
     def train(self, epoch_id):
@@ -30,29 +38,33 @@ class Trainer:
         summary_correct = 0
         summary_loss = 0
 
-        for batch, inputs in enumerate(self.train_set):
-            inputs = [i[1].cuda() for i in inputs]
+        for batch, inputs in enumerate(zip(*self.train_set)):
             self.optimizer.zero_grad()
-            correct, a, p, n = self.model(*inputs)
-            loss = self.criterion(a, p, n)
+            inputs = [[i[1].cuda() for i in j] for j in inputs]
+            a1, a2, a3, p1, p2, p3, n1, n2, n3 = self.model(*inputs)
+            loss1 = self.criterion(a1, p1, n1)
+            loss2 = self.criterion(a2, p2, n2)
+            loss3 = self.criterion(a3, p3, n3)
+            loss = (loss1 + loss2 + loss3) / 3
             summary_loss += loss.item()
-            summary_correct += correct.sum().item()
+            a = (a1 + a2 + a3) / 3
+            p = (p1 + p2 + p3) / 3
+            n = (n1 + n2 + n3) / 3
+            correct = (self.dist(a, p) < self.dist(a, n)).sum().item()
+            summary_correct += correct
             loss.backward()
             self.optimizer.step()
 
             if batch % self.log_interval == 0:
                 print('Epoch {} [{:5.0f}/{} ({:2.1f}%)] Loss: {:.6f} Accuracy: {:.1f}%'.format(
-                    epoch_id, batch * len(self.train_set.dataset) / len(self.train_set), len(self.train_set.dataset),
-                    100. * batch / len(self.train_set), loss.item(), 100. * correct.sum().item() / self.batch_size
+                    epoch_id, batch * self.train_len / self.train_batches, self.train_len,
+                    100. * batch / self.train_batches, loss.item(), 100. * correct / self.batch_size
                 ))
 
         print('[TRAIN] Average loss: {:.6f}, Accuracy: {}/{} ({:2.1f}%)'.format(
-            summary_loss / len(self.train_set.dataset), summary_correct, len(self.train_set.dataset),
-            100. * summary_correct / len(self.train_set.dataset)
-        ))
+            summary_loss / self.train_len, summary_correct, self.train_len, 100. * summary_correct / self.train_len))
 
-        self.plot(Mode.TRAIN, summary_loss / len(self.train_set.dataset),
-                  100. * summary_correct / len(self.train_set.dataset))
+        self.plot(Mode.TRAIN, summary_loss / self.train_len, 100. * summary_correct / self.train_len)
 
     def validate(self):
         self.model.eval()
@@ -60,21 +72,29 @@ class Trainer:
         summary_correct = 0
         loss = 0
 
-        for inputs in self.validate_set:
-            inputs = [i[1].cuda() for i in inputs]
+        for inputs in zip(*self.validate_set):
             with no_grad():
-                correct, anchors, positives, negatives = self.model(*inputs)
-                loss += self.criterion(anchors, positives, negatives).item()
-                summary_correct += correct.sum().item()
+                inputs = [[i[1].cuda() for i in j] for j in inputs]
+                a1, a2, a3, p1, p2, p3, n1, n2, n3 = self.model(*inputs)
+                loss1 = self.criterion(a1, p1, n1)
+                loss2 = self.criterion(a2, p2, n2)
+                loss3 = self.criterion(a3, p3, n3)
+                loss += ((loss1 + loss2 + loss3) / 3).item()
+                a = (a1 + a2 + a3) / 3
+                p = (p1 + p2 + p3) / 3
+                n = (n1 + n2 + n3) / 3
+                correct = (self.dist(a, p) < self.dist(a, n)).sum().item()
+                summary_correct += correct
 
         if self.scheduler is not None:
-            self.scheduler.step(loss / len(self.validate_set.dataset))
+            self.scheduler.step(loss / self.validate_len)
 
-        avg_accuracy = 100. * summary_correct / len(self.validate_set.dataset)
+        avg_accuracy = 100. * summary_correct / self.validate_len
 
         print('[VALIDATE] Average loss: {:.6f}, Accuracy: {}/{} ({:2.1f}%)'.format(
-            loss / len(self.validate_set.dataset), summary_correct, len(self.validate_set.dataset), avg_accuracy))
-        self.plot(Mode.VALIDATE, loss / len(self.validate_set.dataset), avg_accuracy)
+            loss / self.validate_len, summary_correct, self.validate_len, avg_accuracy))
+
+        self.plot(Mode.VALIDATE, loss / self.validate_len, avg_accuracy)
 
         if avg_accuracy > self.best_accuracy:
             self.best_accuracy = avg_accuracy
@@ -84,13 +104,18 @@ class Trainer:
         model.eval()
         ranks = {}
 
-        for i, inputs in enumerate(self.test_set):
-            print(100. * i / len(self.test_set))
-            labels = inputs[0][0]
-            inputs = [i[1].cuda() for i in inputs]
+        for i, inputs in enumerate(zip(*self.test_set)):
+            print(100. * i / self.test_batches)
+            labels = inputs[0][0][0]
+            inputs = [[i[1].cuda() for i in j] for j in inputs]
             with no_grad():
-                correct, anchors, positives, negatives = model(*inputs)
-
+                a1, a2, a3, p1, p2, p3, n1, n2, n3 = self.model(*inputs)
+                a = (a1 + a2 + a3) / 3
+                p = (p1 + p2 + p3) / 3
+                n = (n1 + n2 + n3) / 3
+                correct = self.dist(a, p) < self.dist(a, n)
+                # loss += self.criterion(anchors, positives, negatives).item()
+            # index of highest probability
             for j, label in enumerate(labels):
                 if label not in ranks.keys():
                     ranks[label] = 1
