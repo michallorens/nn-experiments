4e68def917ac3d02213e842895c2357b15654af1
Michał Lorens <Michal.Lorens@pega.com>
2018-09-07 00:31:28 +0200

    CUHK01

--- datasets/cuhk01.py
+++ datasets/cuhk01.py
@@ -6,91 +6,77 @@ from enum import Enum
 from zipfile import ZipFile
 from glob import glob
 
+import copy
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
 
 from datasets.transforms.transforms import Scale
-
-
-class Mode(Enum):
-    TRAIN = 0,
-    VALIDATE = 1,
+from datasets.viper import Mode
 
 
 class CUHK01(Dataset):
     path = './data/campus/'
     zip_path = './data/CUHK01.zip'
 
-    def __init__(self, mode, split, test_transform, train_transform, negative_samples=3):
-        self.mode = mode
-        self.test_transform = test_transform
-        self.train_transform = train_transform
+    def __init__(self, split, transform):
+        self.mode = Mode.TEST
+        self.transform = transform
         self.samples = []
 
         if not osp.exists(self.path) and osp.exists(self.zip_path):
             with ZipFile(self.zip_path, 'r') as zip_file:
                 zip_file.extractall('./data')
 
-        identities = sorted(glob(self.path + '*001.png'))[split[0]:split[1]]
-        identities = [id.split('\\')[1].replace('001.png', '') for id in identities]
+        files_a1 = sorted(glob(self.path + '*001.png'))
+        files_a2 = sorted(glob(self.path + '*002.png'))
+        files_a = files_a1 + files_a1 + files_a1 + files_a2 + files_a2
+        files_b1 = sorted(glob(self.path + '*003.png'))
+        files_b2 = sorted(glob(self.path + '*004.png'))
+        files_b = files_a2 + files_b1 + files_b2 + files_b1 + files_b2
 
-        for id in identities:
-            cam_a = ['{}{:0>4d}{:0>3d}.png'.format(self.path, int(id), i) for i in [1, 2]]
-            cam_b = ['{}{:0>4d}{:0>3d}.png'.format(self.path, int(id), i) for i in [3, 4]]
+        files = sorted(zip(files_a, files_b))
+        random.shuffle(files)
+        files_a[:], files_b[:] = zip(*files)
 
-            if self.mode == Mode.TRAIN:
-                n_ids = random.sample(identities, negative_samples)
-                while id in n_ids:
-                    n_ids = random.sample(identities, negative_samples)
-                cam_n = ['{}{:0>4d}{:0>3d}.png'.format(self.path, int(n_id), random.randint(1, 4)) for n_id in n_ids]
-            else:
-                n_ids = [n_id for n_id in identities if n_id != id]
-                n_ids = itertools.product(n_ids, [1, 2, 3, 4])
-                cam_n = ['{}{:0>4d}{:0>3d}.png'.format(self.path, int(i), j) for i, j in n_ids]
+        a = files_a[split[0]:split[1]]
+        a = {int(path.split('\\')[1].split('.')[0][:-3]) : path for path in a}
+        b = files_b[split[0]:split[1]]
+        b = {int(path.split('\\')[1].split('.')[0][:-3]) : path for path in b}
 
-            triples = itertools.product(cam_a, cam_b, cam_n)
-            if self.mode == Mode.VALIDATE:
-                triples = random.sample(list(triples), negative_samples)
+        for id, cam_a in a.items():
+            cam_a = [id, cam_a]
+            cam_b = [id, b[id]]
+
+            cam_n = [[n_id, b[n_id]] for n_id in a.keys() if n_id != id]
+
+            triples = itertools.product([cam_a], [cam_b], cam_n)
             for anchor, positive, negative in triples:
                 self.samples.append([anchor, positive, negative])
-
-            # [self.samples.append([s, id]) for s in cam_a + cam_b]
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, item):
-        sample = self.samples[item]
-        sample = [Image.open(path) for path in sample]
+        sample = copy.deepcopy(self.samples[item])
 
-        if self.mode == Mode.TRAIN:
-            sample[0] = self.train_transform(sample[0])
-        else:
-            sample[0] = self.test_transform(sample[0])
+        for el in sample:
+            el[1:] = [Image.open(path) for path in el[1:]]
 
-        sample[1] = self.test_transform(sample[1])
-        sample[2] = self.test_transform(sample[2])
+        sample[0][1] = self.transform(sample[0][1])
+
+        sample[1][1] = self.transform(sample[1][1])
+        sample[2][1] = self.transform(sample[2][1])
 
         return sample
 
     @staticmethod
-    def create(split, test_transform=transforms.Compose([
-                    Scale(64),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std=[0.229, 0.224, 0.225])
-                ]),
-               train_transform=transforms.Compose([
-                    transforms.RandomSizedCrop(64),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std=[0.229, 0.224, 0.225])
-                ])):
+    def create(split, transform=transforms.Compose([
+        Scale(64),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])):
 
-        train = CUHK01(Mode.TRAIN, (0, split[0]), test_transform, train_transform, negative_samples=1)
-        val = CUHK01(Mode.VALIDATE, split, test_transform, train_transform, negative_samples=10)
-        test = CUHK01(Mode.VALIDATE, (split[0], None), test_transform, train_transform)
-        return train, val, test
+        test = CUHK01((split[0], None), transform)
+        return None, None, test
--- experiment.py
+++ experiment.py
@@ -7,6 +7,7 @@ import torch.optim.lr_scheduler as scheduler
 from torch import no_grad
 from torch.utils.data import DataLoader
 
+from datasets.cuhk01 import CUHK01
 from datasets.viper import VIPeR, Mode
 from metric.kissme import KISSME
 from models.bkw import BkwNet
@@ -14,10 +15,10 @@ from models.triplet import TripletNet
 from plotter import Plot
 from trainer import Trainer
 
-name = 'shallow-triplet-kissme' + datetime.now().strftime('_%Y-%m-%d_%H%M%S')
+name = 'shallow-triplet-kissme-cuhk' + datetime.now().strftime('_%Y-%m-%d_%H%M%S')
 plot = Plot(name)
 net = TripletNet(BkwNet())
-net.load_state_dict(torch.load('shallow-triplet-64_2018-05-25_202105_model'))
+net.load_state_dict(torch.load('shallow-triplet-128_2018-05-31_100915_model'))
 net.cuda()
 
 train, _, _ = VIPeR.create((316, 380), negative_samples=1)
@@ -52,6 +53,6 @@ criterion = nn.TripletMarginLoss().cuda()
 optimizer = optim.SGD(net.parameters(), lr=1e-2, momentum=0.9)
 scheduler = scheduler.ReduceLROnPlateau(optimizer, patience=1, eps=1e-8, verbose=True)
 
-trainer = Trainer(name, net, VIPeR.create((316, 380)), optimizer, scheduler, criterion, plot,
+trainer = Trainer(name, net, CUHK01.create((0, 971)), optimizer, scheduler, criterion, plot,
                   batch_size=64, log_interval=10, max_epoch=15, metric=metric)
 plot(Mode.TEST, trainer.test(net), None)
